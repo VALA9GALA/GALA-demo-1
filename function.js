4c766edfe006971bcda90c02117e53453271fb84
VALA9GALA <147095924+VALA9GALA@users.noreply.github.com>
2023-10-05 17:10:56 -0400

    worked on randomQuoteAlert

--- function.js
+++ function.js
@@ -1,6 +1,14 @@
+let quote = [
+  "Travel in grace knowing that you are protected by LOVE 💞",
+  "Your actions prove your greatness 🤸‍♀️",
+  "Accept who you are at the moment, but acknowledge who you want to become 🧚‍♀️",
+  "Personal growth lies in the unknown; courage permits you to explore this space 🔮",
+];
+
 function handleSubmit(event) {
   event.preventDefault();
+  let randomQuote = Math.floor(Math.random() * quote.length);
+  let randomQuoteAlert = quote[randomQuote];
+  alert(randomQuoteAlert);
 }
 document.querySelector("#submit-btn").addEventListener("submit", handleSubmit);
-
-alert("");
