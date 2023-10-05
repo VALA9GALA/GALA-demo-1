4aec0457dec3a2b8e0250e8a3e98e5ee3e09e34d
VALA9GALA <147095924+VALA9GALA@users.noreply.github.com>
2023-10-05 17:36:17 -0400

    trying to get the alert to work upon input submit
    also adding styling in css

--- function.js
+++ function.js
@@ -1,14 +1,15 @@
-let quote = [
+let quoteArray = [
   "Travel in grace knowing that you are protected by LOVE 💞",
   "Your actions prove your greatness 🤸‍♀️",
   "Accept who you are at the moment, but acknowledge who you want to become 🧚‍♀️",
   "Personal growth lies in the unknown; courage permits you to explore this space 🔮",
 ];
 
-function handleSubmit(event) {
-  event.preventDefault();
-  let randomQuote = Math.floor(Math.random() * quote.length);
-  let randomQuoteAlert = quote[randomQuote];
+let submitButton = document.querySelector("#submit-btn");
+submitButton.addEventListener("submit", handleSubmit);
+
+function handleSubmit() {
+  let randomQuote = Math.floor(Math.random() * quoteArray.length);
+  let randomQuoteAlert = quoteArray[randomQuote];
   alert(randomQuoteAlert);
 }
-document.querySelector("#submit-btn").addEventListener("submit", handleSubmit);
