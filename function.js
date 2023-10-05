56f09ef59e08bd003694b28c43e180e6ebef95e9
VALA9GALA <147095924+VALA9GALA@users.noreply.github.com>
2023-10-05 19:19:37 -0400

    Lots of style
    styling, spacing, background, image and color!

--- function.js
+++ function.js
@@ -1,14 +1,15 @@
 let quoteArray = [
-  "Travel in grace knowing that you are protected by LOVE 💞",
-  "Your actions prove your greatness 🤸‍♀️",
+  "             Travel in grace knowing that you are protected by LOVE 💞",
+  "                         Your actions prove your greatness 🤸‍♀️",
   "Accept who you are at the moment, but acknowledge who you want to become 🧚‍♀️",
   "Personal growth lies in the unknown; courage permits you to explore this space 🔮",
 ];
 
 let submitButton = document.querySelector("#submit-btn");
-submitButton.addEventListener("submit", handleSubmit);
+submitButton.addEventListener("click", handleSubmit);
 
-function handleSubmit() {
+function handleSubmit(event) {
+  event.preventDefault();
   let randomQuote = Math.floor(Math.random() * quoteArray.length);
   let randomQuoteAlert = quoteArray[randomQuote];
   alert(randomQuoteAlert);
